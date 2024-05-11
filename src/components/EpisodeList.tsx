b7241bd3aa60efa18de4ffce5d4052ab693c8476
unknown <leonideko1@gmail.com>
2024-05-11 22:19:21 +0300

    add styles for episodes

--- src/components/EpisodeList.tsx
+++ src/components/EpisodeList.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import EpisodeDetails from './EpisodeDetails';
+import EpisodeDetails from "./EpisodeDetails";
 interface EpisodeProps {
   id: number;
   name: string;
@@ -14,22 +14,21 @@ interface EpisodeListProps {
 
 const EpisodeList: React.FC<EpisodeListProps> = ({ episodes }) => {
   return (
-    <div>
-      <ul>
-        {episodes.map((episode) => (
-          <li key={episode.id}>
-            <h3>{episode.name}</h3>
-            <p>Air Date: {episode.air_date}</p>
-            <p>Episode: {episode.episode}</p>
-            {/* <ul>
-              {episode.characters.map((character) => (
-                <li key={character}>{character}</li>
-              ))}
-            </ul> */}
-             <EpisodeDetails episodeId={episode.id} />
-          </li>
-        ))}
-      </ul>
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+      {episodes.map((episode) => (
+        <div
+          key={episode.id}
+          className="bg-white shadow-md rounded-lg overflow-hidden"
+        >
+          <div className="h-48 bg-gray-300"></div>
+          <div className="p-4">
+            <h3 className="text-lg font-semibold mb-2">{episode.name}</h3>
+            <p className="text-gray-600 mb-2">Air Date: {episode.air_date}</p>
+            <p className="text-gray-600 mb-4">Episode: {episode.episode}</p>
+            <EpisodeDetails episodeId={episode.id} />
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
