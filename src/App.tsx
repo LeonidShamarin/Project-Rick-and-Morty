391db01f0e93aabe1534d9c1cb3573af5708b2c8
unknown <leonideko1@gmail.com>
2024-05-11 17:54:42 +0300

    initial project

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import EpisodesPage from "./pages/EpisodesPage";
+import CharactersPage from "./pages/CharactersPage";
+import LocationsPage from "./pages/LocationsPage";
+import Footer from "./components/Footer";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Episodes</Link>
+            </li>
+            <li>
+              <Link to="/characters">Characters</Link>
+            </li>
+            <li>
+              <Link to="/locations">Locations</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<EpisodesPage />} />
+
+          <Route path="/characters" element={<CharactersPage />} />
+
+          <Route path="/locations" element={<LocationsPage />} />
+        </Routes>
+
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+export const purge = ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'];
+export const darkMode = false;
+export const theme = {
+  extend: {},
+};
+export const variants = {
+  extend: {},
+};
+export const plugins = [];
