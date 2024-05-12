c261156bd614bf72e58e2358a834e0a8d39d5b13
unknown <leonideko1@gmail.com>
2024-05-12 10:00:36 +0300

    add style for locationlist

--- src/components/LocationList.tsx
+++ src/components/LocationList.tsx
@@ -13,16 +13,25 @@ interface LocationListProps {
 
 const LocationList: React.FC<LocationListProps> = ({ locations }) => {
   return (
-    <div>
-      <ul>
-        {locations.map((location) => (
-          <li key={location.id}>
-            <h3>{location.name}</h3>
-            <p>Type: {location.type}</p>
-            <p>Dimension: {location.dimension}</p>
-          </li>
-        ))}
-      </ul>
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+      {locations.map((location) => (
+        <div
+          key={location.id}
+          className="bg-white shadow-md rounded-lg overflow-hidden"
+        >
+          <div className="h-48 bg-gray-300"></div>
+          <div className="p-4">
+            <h3 className="text-lg font-semibold mb-2">{location.name}</h3>
+            <p className="text-gray-600 mb-1">
+              <span className="font-semibold">Type:</span> {location.type}
+            </p>
+            <p className="text-gray-600">
+              <span className="font-semibold">Dimension:</span>
+              {location.dimension}
+            </p>
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
